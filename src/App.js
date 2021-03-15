be8789026be568dca249b55655f18e8e6d4a44a9
Zachary Inkster <z.j.inkster@gmail.com>
2021-03-15 13:46:50 -0500

    Reformatted files. Fixed error in movies components -changed movies to movie- and rendered table on page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Movies from "./components/movies";
+import React, { Component } from 'react';
 
-
-function App() {
-  return (
-    <main className="container">
-      <Movies />
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+        <Movies />
+      </main>
+    );
+  }
 }
 
 export default App;
