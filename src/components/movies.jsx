f9941cd705470b2718e57fb5316f1f32eead30d6
Zachary Inkster <z.j.inkster@gmail.com>
2021-03-15 14:02:05 -0500

    Added functionality to delete function to remove the movie selected. Sets state correspondingly

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,7 +3,12 @@ import { getMovies } from "../services/services/fakeMovieService";
 
 class Movies extends Component {
     state = { 
-        movie: getMovies()
+        movies: getMovies()
+     };
+
+     deleteMovie = (movie) =>{
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies});
      };
 
     render() { 
@@ -14,15 +19,17 @@ class Movies extends Component {
                     <th>Genre</th>
                     <th>Stock</th>
                     <th>Rate</th>
+                    <th />
                 </tr>
             </thead>
             <tbody>
-                {this.state.movie.map(movie => (
-                <tr>
+                {this.state.movies.map(movie => (
+                <tr key={movie._id}>
                     <td>{movie.title}</td>
                     <td>{movie.genre.name}</td>
                     <td>{movie.numberInStock}</td>
                     <td>{movie.dailyRentalRate}</td>
+                    <td><button onClick={() => this.deleteMovie(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                 </tr>
                 ))}
             </tbody>
