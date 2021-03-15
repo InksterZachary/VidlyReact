ef26d4fd6564fda16a3d04ac68f83f7bdd9a29e6
Zachary Inkster <z.j.inkster@gmail.com>
2021-03-15 11:30:43 -0500

    Libraries installed, added components folder, and fake api's for reference

---
+++ src/movies.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+// import { getMovies } from "../src/services/services/fakeMovieService";
+
+
+class Movies extends Component {
+    state = {};
+
+    // handleDelete = movie => {
+
+    // };
+
+    render(){
+        return <h2>Movies Component</h2>;
+    }
+}
+
+export default Movies;
